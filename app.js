bafde82817dcd3b92a5b7d178a2fc7fb05c687ff
cartagoReminiscence <abjos7@protonmail.com>
2022-09-18 03:19:14 -0500

    Reemplaza letra acertada por underscore en pantalla, reconoce las etiquetas que necesitan cambiar. Nombre de variables renombradas y logs eliminados.

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ Cómo presentar el trabajo final (según lo que va a considerar el profesor):
 To do:
     - Agregar un universo de palabras candidatas a ser adivinadas [check]
     - escoger aleatoriamente una palabra del array y asignar (este string) a la variable "palabra" [check]
+    - Agregar el boton "Pedir una pista" [check]
+    - Muestre en pantalla si perdí o gané [check]
 
     - Mostrar en pantalla los acumuladores (usar metodo "document.querySelector") como por ejm:
     las variables "totalCoincidencias" convertir de "_ _ _ _ _ _" a "_ N N _ _ _", 
     "totalFallas" -> "Numero de intentos restantes", las pistas
-
-    - Agregar el boton "Pedir una pista" [check]
-    - 
 
 */
 
@@ -60,27 +59,36 @@ let [ palabra, textoPista ] = escogerPalabraYPista();//Asignar la palabra a adiv
 // Genera la misma cantidad de caracteres del string "palabra" con "_" caracteres
 const guionBajoPalabraDesc = () => {
     let divPalabraDesc = document.querySelector('#palabra-desconocida');//etiqueta div id="palabra-desconocida"
+    let parrafoSetLetraDesconocida = document.createElement('p');//etiqueta p id="set-letra-desconocida"
+    parrafoSetLetraDesconocida.id = "set-letra-desconocida";
     // Agregar "_" caracteres a div id="palabra-desconocida"
     let len = palabra.length;
     for(let i = 0; i < len; i++){
-        let spanLetraDesc = document.createElement('span');//etiqueta span id="letra-desconocida"
-        spanLetraDesc.id = 'letra-desconocida';
+        let spanLetraDesc = document.createElement('span');//crear etiqueta span id="letra-desconocida"
+        spanLetraDesc.className = 'letra-desconocida';
+        spanLetraDesc.id = `span-${ i }`;
         spanLetraDesc.innerText = '__';
 
-        divPalabraDesc.innerHTML += ' ';//espacio entre "spanLetraDesc" elementos
-        divPalabraDesc.append( spanLetraDesc );// Agregar a "divPalabraDesc"
+        parrafoSetLetraDesconocida.innerHTML += ' ';//espacio entre "spanLetraDesc" elementos
+        parrafoSetLetraDesconocida.append( spanLetraDesc );// Agregar a "parrafoSetLetraDesconocida"
     }
+    divPalabraDesc.append(parrafoSetLetraDesconocida);//agregar a "divPalabraDesc"
 };
 
 // Limpia la pantalla de "_" caracteres
 const limpiarPalabraDesc = () => {
     let divPalabraDesc = document.querySelector('#palabra-desconocida');
-    while( divPalabraDesc.childElementCount != 0 ){
-        let firstDivPalabraDesc = divPalabraDesc.firstElementChild;
+    let firstDivPalabraDesc = divPalabraDesc.firstElementChild;
+    if( divPalabraDesc.childElementCount != 0 )
         divPalabraDesc.removeChild( firstDivPalabraDesc );
-    }
 };
 
+// Reemplazar "_" por palabras encontradas
+let agregarCaracteresASetLetraDesc = (arreglo) => {
+    let parrafoSetLetraDesconocida = document.querySelector('p');//etiqueta p id="set-letra-desconocida"
+};
+
+/*              */ 
 function validacion(palabra, caracter){
     let tam = palabra.length;//tamaño de la palabra
     let contador = 0;//numero de coincidencias
@@ -102,6 +110,23 @@ const contadorImgSet = num => {
     return num + 1;
 };
 
+/*     funcion reconocer posiociones      */
+let posiciones;
+let reconocerPos = ( letra ) => {
+    posiciones = [];
+    let iterador = palabra.matchAll(letra);//reconoce la posicion de todas las coincidencias de la 'letra'
+    for(let i of iterador) posiciones.push(i.index);
+};
+
+/** */
+let mostrarEnSetLetraDesc = () => {
+    let parrafo = document.querySelector('#set-letra-desconocida');
+    let letra = input.value.toUpperCase();
+    for(let pos of posiciones){
+        let span = parrafo.children[pos];
+        span.innerText = letra;
+    }
+};
 
 // Ingresar una palabra
 let input = document.querySelector('input');
@@ -115,42 +140,51 @@ let btnIngresar = document.querySelector('#ingresar');
 const ingresar = () => {
     //
     let letra = input.value;
-    
-    // console.log({ valor_input: input.value });
-    
+    // 
     letra = letra.toUpperCase();
     let { estado, contador: coincidencias } = validacion(palabra, letra);
     
     if(estado && uniendoPalabra.indexOf(letra) === -1){
         totalCoincidencias += coincidencias;
-        for(let cont = 0; cont < coincidencias; cont++) uniendoPalabra.push(letra);
-        console.log("Acertaste");
+        for(let cont = 0; cont < coincidencias; cont++){
+            uniendoPalabra.push(letra);
+        }
+        reconocerPos(letra);
+        mostrarEnSetLetraDesc();
     }
     else{
         cambiarImg();
         totalFallas -= 1;
-        console.log("Fallaste!!!");
     }
-    
-    console.log({ totalCoincidencias });
     
     /*  Determina si Ganaste o perdiste el juego   */
     if( totalFallas === 0 ){
         btnIngresar.disabled = true;
         btnPedirPista.disabled = true;
-        console.log("===================");
-        console.log("PERDISTE!!!!!");
-        console.log("===================");
+        /*              */
+        limpiarPalabraDesc();//Limpiar pantalla
+        // Perdiste en pantalla
+        let parrafoPerdiste = document.createElement('p');
+        parrafoPerdiste.className = 'letra-desconocida';
+        parrafoPerdiste.innerText = `Perdiste la palabra es ${ palabra } :(`;
+        
+        let divPalabraDesc = document.querySelector('#palabra-desconocida');//etiqueta div id="palabra-desconocida"
+        divPalabraDesc.append( parrafoPerdiste );
     }
 
     if( totalCoincidencias === palabra.length ){
         btnIngresar.disabled = true;
         btnPedirPista.disabled = true;
-        console.log("===================");
-        console.log("GANASTEEEE!!!!!");
-        console.log("===================");
+         /*              */
+        limpiarPalabraDesc();//Limpiar pantalla
+        // Perdiste en pantalla
+        let parrafoPerdiste = document.createElement('p');
+        parrafoPerdiste.className = 'letra-desconocida';
+        parrafoPerdiste.innerText = `GANASTE:\t la palabra es ${ palabra }!`;//reemplazar por el atributo innerText de la etiqueta "div"
+        
+        let divPalabraDesc = document.querySelector('#palabra-desconocida');//etiqueta div id="palabra-desconocida"
+        divPalabraDesc.append( parrafoPerdiste );
     }
-    
 };
 
 /*  Pedir Pista  */
@@ -183,10 +217,11 @@ if( picture.childElementCount != 0 ){
 
 let picture = document.querySelector('picture');
 const iniciar = () => {
+    posiciones = [];//reiniciar las posiciones
     [ palabra, textoPista ] = escogerPalabraYPista();//Asignar la palabra a adivinar y su pista respectiva
     limpiarPalabraDesc();//limpia la pantalla de "_" caracteres
     guionBajoPalabraDesc();// añade "_" caracteres 
-    // console.log({ len_palabra: palabra.length });//corroboración del tamaño de "palabra-desconocida"
+
     limpiarPista();//limpia en pantalla la pista de la partida anterior
 
     btnPedirPista.disabled = false;//habilitar boton al inicio del juego
@@ -201,14 +236,12 @@ const iniciar = () => {
     img = document.createElement('img');
     img.src = `./img/ahorcado_2.jpeg`;
     picture.append(img);
-    // console.log({ contador: num, hijos: picture.childElementCount });
 };
 
 /* Cambiar el estado del juego */
 const cambiarImg = () => {
     num = contadorImgSet(num);
-    // console.log({ contador: num });
-
+    //
     picture.removeChild(picture.firstElementChild);
 
     let img = document.createElement('img');
